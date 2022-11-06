352e3d9c7310274a8686e590fb332ad0921cf6c9
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2022-11-06 09:10:22 -0400

    add METHOD GetMapping

---
+++ src/main/java/com/todocodeacademy/primerproyecto/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.todocodeacademy.primerproyecto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hellow")
+    public String hellow(){
+        return "hellow World";
+    }
+
+    @GetMapping("/hello/{nombre}/{edad}/{profesion}")
+    public String sayHello(@PathVariable String nombre,
+                           @PathVariable int edad,
+                           @PathVariable String profesion)
+    {
+        return "El nombre es: " + nombre + "\n" +
+                "La edad es: " + edad + "\n" +
+                "La profesion es: " + profesion;
+    }
+}
